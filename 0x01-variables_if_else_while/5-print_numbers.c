131f7c49f30961b6e010fc3e06dbf4fd3f606485
amriyounes <amriyounes72@gmail.com>
2023-09-08 12:24:24 -0700

    0x01-varibales_Edit Betty coding style

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - main block
+ * main - Entry point of the program
+ *
  * Description: Print all numbers of base 10, starting from 0.
- * Return: 0
+ *
+ * Return: Always 0 (indicating successful execution)
  */
-
 int main(void)
 {
-	int x = 0;
+        int x = 0;
 
-	for (; x < 10; x++) 
-		printf("%d", x);
+        for (; x < 10; x++)
+                printf("%d", x);
 
-	printf("\n");
-	return (0);
+        printf("\n");
+        return (0);
 }
