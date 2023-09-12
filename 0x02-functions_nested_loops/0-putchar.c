e6cf8e21b172243ff837e0bfd9950f0528c54ebc
amriyounes <amriyounes72@gmail.com>
2023-09-12 12:40:54 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the string "_putchar" followed by a newline
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char str[] = "_putchar";
+    int i = 0;
+
+    for (; str[i] != '\0'; i++)
+        putchar(str[i]);
+    putchar('\n');
+
+    return (0);
+}
