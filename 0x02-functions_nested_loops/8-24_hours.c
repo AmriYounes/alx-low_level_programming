e23e9fbcbce80c1e0a0589665a5402fc62a69dc8
amriyounes <amriyounes72@gmail.com>
2023-09-12 16:30:37 -0700

    0x02_Task 08

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of a day in 24-hour format
+ */
+
+void jack_bauer(void)
+{
+	int x= 0 ;
+        int y= 0 ;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
