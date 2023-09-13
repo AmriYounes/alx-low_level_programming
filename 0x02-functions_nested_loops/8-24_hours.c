9d297c908d97f655291c39d727bbef8e5ad13833
amriyounes <amriyounes72@gmail.com>
2023-09-13 16:52:12 -0700

    0x02 Edit Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,19 +6,19 @@
 
 void jack_bauer(void)
 {
-	int x= 0 ;
-        int y= 0 ;
+int x = 0;
+int y = 0;
 
-	for (x = 0; x < 24; x++)
-	{
-		for (y = 0; y < 60; y++)
-		{
-			_putchar((x / 10) + '0');
-			_putchar((x % 10) + '0');
-			_putchar(':');
-			_putchar((y / 10) + '0');
-			_putchar((y % 10) + '0');
-			_putchar('\n');
-		}
-	}
+for (x = 0; x < 24; x++)
+{
+for (y = 0; y < 60; y++)
+{
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+_putchar(':');
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+_putchar('\n');
+}
+}
 }
