854771d3341749f50677cf34742c8bc7ce851f39
amriyounes <amriyounes72@gmail.com>
2023-09-13 16:34:51 -0700

    0x02 Edit Task 02

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet ten times, each followed by a newline
+ * print_alphabet_x10 - Prints the lowercase alphabet 10X.
  */
 
 void print_alphabet_x10(void)
