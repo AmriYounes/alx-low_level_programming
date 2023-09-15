402d72189c42589eddb6b1221f59218a2803e332
amriyounes <amriyounes72@gmail.com>
2023-09-15 13:15:32 -0700

    0x04 task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check for digits (0 through 9)
+ * @c: int type param
+ * Return: 1 if digit, else 0
+ */
+
+int _isdigit(int c)
+{
+
+return ((c >= 48 && c <= 57) ? 1 : 0);
+}
