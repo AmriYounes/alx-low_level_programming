b8dd4e875a3334efde45446c33a073074e19b798
amriyounes <amriyounes72@gmail.com>
2023-09-19 16:46:01 -0700

    0x05 Task08 (Edit)

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,10 +8,18 @@
  */
 void print_array(int *a, int n)
 {
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
 	int i;
 
 	for (i = 0; i < n - 1; i++)
+	{
 		printf("%d, ", a[i]);
+	}
 	printf("%d\n", a[i]);
 }
 
