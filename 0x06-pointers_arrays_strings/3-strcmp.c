2ae9376b61e2a150d87b0e1bddc1c5b84b85e4d6
amriyounes <amriyounes72@gmail.com>
2023-09-21 19:31:49 -0700

    0x06 Task03

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: Pointer to the first string.
+ * @s2: Pointer to the second string.
+ *
+ * Return: <0 value if s1<s2, >0 value if s1>s2, 0 if they are equal.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+
+	return (0);
+}
+
