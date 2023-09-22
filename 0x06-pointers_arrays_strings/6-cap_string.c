92d763c379d646a1e6a82946a58240a93b9bee49
amriyounes <amriyounes72@gmail.com>
2023-09-22 08:19:28 -0700

    0x06 Task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes String after certain punctuation marks.
+ * @s: Pointer to the string.
+ *
+ * Return: Pointer to the modified string.
+ */
+char *cap_string(char *s)
+{
+	char *p = s;
+
+	while (*p != '\0')
+	{
+		if (*p == ',' || *p == ';' || *p == '.' || *p == '!' ||
+		*p == '?' || *p == '"' || *p == '(' || *p == ')' || *p == '{'
+		|| *p == '}')
+		{
+			if (*(p + 1) >= 'a' && *(p + 1) <= 'z')
+				*(p + 1) -= 32;
+		}
+		p++;
+	}
+	return (s);
+}
+
